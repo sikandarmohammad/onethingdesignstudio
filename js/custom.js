5974716fe07a97505e99e29805b526d288fabd84
Aniruddh Agarwal <aniruddh@externlabs.com>
2022-03-09 16:02:01 +0530

    Update Portfolio

--- js/custom.js
+++ js/custom.js
@@ -49,3 +49,20 @@ cursorScale.forEach(link => {
         }
     });
 });
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray(".panel").forEach((panel, i) => {
+  ScrollTrigger.create({
+    trigger: panel,
+    start: "top top", 
+    pin: true, 
+    pinSpacing: false 
+  });
+});
+
+
+ScrollTrigger.create({
+  snap: 1 / 4 // snap whole page to the closest section!
+});
+
